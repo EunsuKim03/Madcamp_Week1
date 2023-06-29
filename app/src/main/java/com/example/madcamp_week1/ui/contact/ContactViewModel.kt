7fec6ba8a7572c488a75f04d58746c5892c7cc2d
cactus-y <youseokwon1667@gmail.com>
2023-06-29 17:16:04 +0900

    Modified variable names

--- app/src/main/java/com/example/madcamp_week1/ui/contact/ContactViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/contact/ContactViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.madcamp_week1.ui.dashboard
+package com.example.madcamp_week1.ui.contact
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ContactViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is contact Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/madcamp_week1/ui/likes/LikesViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/likes/LikesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.madcamp_week1.ui.home
+package com.example.madcamp_week1.ui.likes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class LikesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is likes Fragment"
     }
     val text: LiveData<String> = _text
 }
