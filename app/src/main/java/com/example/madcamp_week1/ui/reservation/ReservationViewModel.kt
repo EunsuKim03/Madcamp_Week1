4ec58b932457ab7cefe450531e3d409a8f96863a
EunsuKim03 <es4761@kaist.ac.kr>
2023-06-30 13:24:13 +0900

    tab3: reservation

--- app/src/main/java/com/example/madcamp_week1/ui/reservation/ReservationViewModel.kt
+++ app/src/main/java/com/example/madcamp_week1/ui/reservation/ReservationViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.madcamp_week1.ui.likes
+package com.example.madcamp_week1.ui.reservation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LikesViewModel : ViewModel() {
+class ReservationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is likes Fragment"
