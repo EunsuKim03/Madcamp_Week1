82024842d8b6483fff922fada184075e7df33463
EunsuKim03 <es4761@kaist.ac.kr>
2023-07-01 18:25:30 +0900

    Swiper added & Gallery margin modified

--- app/src/main/java/com/example/madcamp_week1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_week1/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.madcamp_week1
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.madcamp_week1.databinding.ActivityMainBinding
+import com.example.madcamp_week1.ui.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,17 +22,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navi_contact, R.id.navi_gallery, R.id.navi_reservation
-            )
+        binding.pager.adapter = ViewPagerAdapter(this)
+
+        binding.pager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.navView.menu.getItem(position).isChecked = true
+                }
+            }
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navi_contact -> {
+                    binding.pager.currentItem = 0
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navi_gallery -> {
+                    binding.pager.currentItem = 1
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navi_reservation -> {
+                    binding.pager.currentItem = 2
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    binding.pager.currentItem = 3
+                    return@setOnItemSelectedListener false
+                }
+            }
+        }
+
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navi_contact, R.id.navi_gallery, R.id.navi_reservation
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
+
+
 }
+
+
